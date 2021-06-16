c4dc82e4eb28428c8fb953e7967487689182325c
teodoragrubor <teodora.grubor@gmail.com>
2021-06-16 19:47:39 +0200

    Sveeee aaaa

---
+++ src/main/resources/static/precache-manifest.187346068a98024ab92bfcadde4f610b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e4d0a907d18718c0bed",
+    "url": "/static/css/main.1f0f9eee.chunk.css"
+  },
+  {
+    "revision": "3e4d0a907d18718c0bed",
+    "url": "/static/js/main.3e4d0a90.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "71b95f132d08d3536164",
+    "url": "/static/js/2.71b95f13.chunk.js"
+  },
+  {
+    "revision": "943911f8de27ffd757b68fa0bf35c8f4",
+    "url": "/static/media/unnamed.943911f8.jpg"
+  },
+  {
+    "revision": "30b846cd95d1a14ca8a2b4f84f410fa7",
+    "url": "/static/media/logo.30b846cd.png"
+  },
+  {
+    "revision": "3f140c979378138fb337d3643efd2cf1",
+    "url": "/index.html"
+  }
+];
